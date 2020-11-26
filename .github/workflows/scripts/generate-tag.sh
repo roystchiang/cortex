0aed50f53f1685c5ae4dbeb910277f486b3279c6
Rahul Kumar Saini <rahul-kumar-saini@users.noreply.github.com>
2020-11-25 22:47:14 -0500

    generate-tag creates master-<sha> or <tag>-<sha>

--- .github/workflows/scripts/generate-tag.sh
+++ .github/workflows/scripts/generate-tag.sh
@@ -3,7 +3,8 @@
 set -x
 REF="$(cut -d'/' -f 3 <<< $GH_REF)"
 [[ $REF = "master" ]] && TAG="" || TAG="$REF"
-WORKING_SUFFIX=$(if git status --porcelain | grep -qE '^(?:[^?][^ ]|[^ ][^?])\s'; then echo "-WIP"; else echo ""; fi)
 BRANCH_PREFIX=$(git rev-parse --abbrev-ref HEAD)
-GIT_COMMIT="${BRANCH_PREFIX//\//-}-$(git rev-parse --short HEAD)$WORKING_SUFFIX"
-echo "DOCKER_IMAGE_TAG=${TAG:-$GIT_COMMIT}" >> $GITHUB_ENV
+SHA=$(git rev-parse --short HEAD)
+GIT_COMMIT="${BRANCH_PREFIX//\//-}-$SHA"
+[[ $TAG = "" ]] && TAG="$GIT_COMMIT" || TAG="$TAG-$SHA"
+echo "DOCKER_IMAGE_TAG=$TAG" >> $GITHUB_ENV
